df1f9e303ba5fbc2fa9bae6eb2e29f4638a6bfbf
Olukoya iyiola <61966129+dynamikcodez@users.noreply.github.com>
2021-11-03 16:33:29 +0100

    reduced hard coding

--- main.py
+++ main.py
@@ -1,97 +1,41 @@
-company_profile_links = [
-'https://www.businesslist.com.ng/company/256648/ecozar',
-'https://www.businesslist.com.ng/company/264799/jules-construction',
-'https://www.businesslist.com.ng/company/253773/show-gear',
-'https://www.businesslist.com.ng/company/264333/anttention-media-africa-limited',
-'https://www.businesslist.com.ng/company/265945/addurrah-creations',
-'https://www.businesslist.com.ng/company/254049/chogon-bedbugs-fumigation-services',
-'https://www.businesslist.com.ng/company/190403/chogon-guards-and-safety-limited',
-'https://www.businesslist.com.ng/company/260191/bgn-photography',
-'https://www.businesslist.com.ng/company/261076/hephzibah-tutors',
-'https://www.businesslist.com.ng/company/259444/lextorah-school-of-languages',
-'https://www.businesslist.com.ng/company/197011/nekta-group',
-'https://www.businesslist.com.ng/company/260933/rocaprosthetics-ortho-care-enterprises',
-'https://www.businesslist.com.ng/company/266287/rain-international-rain-soul-rain-core-rain-bend',
-'https://www.businesslist.com.ng/company/266106/okposi-christian-graduateundergraduate-fellowship',
-'https://www.businesslist.com.ng/company/261458/gotrack-ltd',
-'https://www.businesslist.com.ng/company/259449/phillips-olly-global-enterprise',
-'https://www.businesslist.com.ng/company/258330/samsonco',
-'https://www.businesslist.com.ng/company/252982/smartedge-business-solutions-ltd',
-'https://www.businesslist.com.ng/company/253384/must-do-properties',
-'https://www.businesslist.com.ng/company/267137/gakaliz-travels-and-tour-ltd',
-'https://www.businesslist.com.ng/company/99875/enplan-group',
-'https://www.businesslist.com.ng/company/259447/og-limited',
-'https://www.businesslist.com.ng/company/190182/deep-delve-research-concepts',
-'https://www.businesslist.com.ng/company/264787/pillyrig',
-'https://www.businesslist.com.ng/company/255528/eko-pixel',
-'https://www.businesslist.com.ng/company/149715/delak-pc-repairs',
-'https://www.businesslist.com.ng/company/260067/zippro-system-limited',
-'https://www.businesslist.com.ng/company/196458/cush-stocktakersinventory',
-'https://www.businesslist.com.ng/company/190959/blissfield-dental-clinic',
-'https://www.businesslist.com.ng/company/257384/anochemical-cosmetics-industries-limited',
-'https://www.businesslist.com.ng/company/111568/pentagon-plastic-industries-ltd',
-'https://www.businesslist.com.ng/company/252635/lovestruck-cakes-gifts-foodsevents',
-'https://www.businesslist.com.ng/company/248217/alashe-baba-ijebu-lotto',
-'https://www.businesslist.com.ng/company/258629/dy-language-solutions',
-'https://www.businesslist.com.ng/company/267792/braingrace-repatriationfuneral-home',
-'https://www.businesslist.com.ng/company/196912/chogon-cleaning-and-fumigation-services',
-'https://www.businesslist.com.ng/company/265088/chogon-guards-and-safety-limited',
-'https://www.businesslist.com.ng/company/267810/colossal-and-quality-build-limited',
-'https://www.businesslist.com.ng/company/263203/destinali',
-'https://www.businesslist.com.ng/company/258873/devtek-tanks',
-'https://www.businesslist.com.ng/company/137617/doglordkennels-services',
-'https://www.businesslist.com.ng/company/258433/healthview-medical-laboratory',
-'https://www.businesslist.com.ng/company/256258/inspiring-careers-and-solutions',
-'https://www.businesslist.com.ng/company/265115/itm-network-limited',
-'https://www.businesslist.com.ng/company/257082/ju-rahama-nigeria-limited',
-'https://www.businesslist.com.ng/company/256903/nnew-virtual-office',
-'https://www.businesslist.com.ng/company/257911/oriflame-consultant-tricia',
-'https://www.businesslist.com.ng/company/255783/slite',
-'https://www.businesslist.com.ng/company/196624/biblical-university',
-'https://www.businesslist.com.ng/company/264957/chogon-fumigation-services',
-'https://www.businesslist.com.ng/company/260860/eazytek-solutions',
-'https://www.businesslist.com.ng/company/260149/leben-travels-and-tours',
-'https://www.businesslist.com.ng/company/267256/odigital',
-'https://www.businesslist.com.ng/company/267465/upwise-digital',
-'https://www.businesslist.com.ng/company/267840/iten-travels-and-migrations-limited',
-'https://www.businesslist.com.ng/company/264602/michael-king-ent',
-'https://www.businesslist.com.ng/company/256100/events-chronicles',
-'https://www.businesslist.com.ng/company/267260/vision-planet-eye-clinic',
-'https://www.businesslist.com.ng/company/266007/aom-integrated',
-'https://www.businesslist.com.ng/company/267801/dmi-digital-marketing-agency',
-'https://www.businesslist.com.ng/company/258399/fsnlink-communications',
-'https://www.businesslist.com.ng/company/263812/loochi-collections',
-'https://www.businesslist.com.ng/company/264463/ukamistic-janitorials-and-services',
-'https://www.businesslist.com.ng/company/253047/aerovessel-travels-and-tours-ltd',
-'https://www.businesslist.com.ng/company/267356/printika-nigeria',
-'https://www.businesslist.com.ng/company/253570/stop-leak-nigeria-limited',
-'https://www.businesslist.com.ng/company/256076/tee-concept-furnitures',
-'https://www.businesslist.com.ng/company/265814/tesgen-power-service-limted',
-'https://www.businesslist.com.ng/company/260930/cleanscope-international-limited',
-'https://www.businesslist.com.ng/company/266333/cubenox-limited',
-'https://www.businesslist.com.ng/company/261745/justforex',
-'https://www.businesslist.com.ng/company/261378/matto-tech-computers-service',
-'https://www.businesslist.com.ng/company/253176/multsol-technology',
-'https://www.businesslist.com.ng/company/263130/nuban-beauty-ltd',
-'https://www.businesslist.com.ng/company/267596/sharp-autos',
-'https://www.businesslist.com.ng/company/252789/wwwfragrancescomng',
-'https://www.businesslist.com.ng/company/265874/palmacedar-cleaning-limited',
-'https://www.businesslist.com.ng/company/262213/rio-weighing-systems-limited',
-'https://www.businesslist.com.ng/company/101832/alpha-pharmacystores-limited',
-'https://www.businesslist.com.ng/company/196687/hurs-concepts',
-'https://www.businesslist.com.ng/company/196669/oven-delight-catering-services',
-'https://www.businesslist.com.ng/company/253635/solar-wizard-nigeria',
-'https://www.businesslist.com.ng/company/190690/selfworth-organization-for-women-development',
-'https://www.businesslist.com.ng/company/263352/amobile-toilets',
-'https://www.businesslist.com.ng/company/258623/bhl-haulage-and-logistics',
-'https://www.businesslist.com.ng/company/261576/totalview-media',
-'https://www.businesslist.com.ng/company/263857/carton-master',
-'https://www.businesslist.com.ng/company/190137/automated-accounting-solutioninfo-tech-ltd']
-
-
+from bs4.element import Tag
 import requests
 from bs4 import BeautifulSoup
- 
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+# browser = webdriver.Chrome(executable_path="./drivers/chromedriver")
+# browser.get('https://www.businesslist.com.ng/location/lagos/3')
+index = 1
+# URL = f'https://www.businesslist.com.ng/location/lagos/{index}'
+BASE_URL = 'https://www.businesslist.com.ng'
+web_pages = []
+for i in range(40):
+    index +=1
+    URL = f'https://www.businesslist.com.ng/location/lagos/{index}'
+    print('Going through index ', index)
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    companies = (soup.find_all('h4'))
+    companies_link_list = []
+    for company in companies:
+        companies_link_list.append(company.find('a').get('href'))
+
+
+    # print(companies)
+    # print(companies_link_list)
+
+    # print(BASE_URL+companies_link_list[1])
+
+    
+    for link in companies_link_list:
+        web_page = BASE_URL+link
+        web_pages.append(web_page)
+
+import xlsxwriter
+from xlsxwriter import workbook
+
+wb = xlsxwriter.Workbook("0000 Company details.xlsx")
 
 company_dict = {}
 names = []
@@ -100,7 +44,7 @@ numbers = []
 sites = []
 i = 0
 
-for x in company_profile_links:
+for x in web_pages:
     i +=1
     print("Parsing site ", i)
     URL = x
@@ -123,13 +67,34 @@ for x in company_profile_links:
         for links in company_site:
             company_link = (links.get('href'))
     except:
-        company_name = "Not Listed"
+        company_link = "Not Listed"
 
     names.append(company_name)
     adresses.append(company_adress)
     numbers.append(company_number)
     sites.append(company_link)
 
+worksheet = wb.add_worksheet()
+row = 0
+column = 0
+
+for item in names:
+    worksheet.write(row, column, item)
+    row+=1
+row = 0
+for item in adresses:
+    worksheet.write(row,1,item)
+    row+=1
+row = 0
+for item in numbers:
+    worksheet.write(row,2,item)
+    row+=1
+row = 0
+for item in sites:
+    worksheet.write(row,3,item)
+    row+=1
+
+wb.close()
 
 print("NAMES", names)
 print("ADRESSES", adresses)
@@ -146,3 +111,7 @@ for x in numbers:
     if x == "Not Found":
         i +=1
 print(i)
+
+
+# time.sleep(10)
+# browser.quit()
